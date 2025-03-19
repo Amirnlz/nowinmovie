19c74ff9312613506c9a47f8d8d886ed1c78ea81
Amir <amiirnlz79@gmail.com>
2025-03-20 02:55:29 +0330

    Feat: add dependencies of AndroidApplicationConventionPlugin

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -1,10 +1,12 @@
 import com.amirnlz.nowinmovie.Config
 import com.amirnlz.nowinmovie.configureFlavors
 import com.amirnlz.nowinmovie.configureKotlinAndroid
+import com.amirnlz.nowinmovie.libs
 import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -26,6 +28,24 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 }
                 testOptions.animationsDisabled = true
                 configureFlavors(this)
+            }
+            dependencies {
+                add(
+                    "implementation",
+                    libs.findLibrary("hilt.navigation.compose").get(),
+                )
+                val subprojects = project
+                    .rootProject
+                    .subprojects
+
+                subprojects.filter { it.path.startsWith(":core:domain:", false) }
+                    .forEach { add("implementation", project(it.path)) }
+
+                subprojects.filter { it.path.startsWith(":core:data:", false) }
+                    .forEach { add("implementation", project(it.path)) }
+
+                subprojects.filter { it.path.startsWith(":feature:", false) }
+                    .forEach { add("implementation", project(it.path)) }
             }
         }
     }
