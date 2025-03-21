03e5e4b0e148ab23acd4f42f07acec4557d736bf
Amir <amiirnlz79@gmail.com>
2025-03-21 15:49:46 +0330

    Chore: add coroutine to data module

--- core/data/movie/src/main/java/com/amirnlz/core/data/movie/data_source/remote/MovieRemoteDataSource.kt
+++ core/data/movie/src/main/java/com/amirnlz/core/data/movie/data_source/remote/MovieRemoteDataSource.kt
@@ -3,44 +3,40 @@ package com.amirnlz.core.data.movie.data_source.remote
 import com.amirnlz.core.network.api.MovieApiService
 import com.amirnlz.core.network.dto.MovieDetailsDto
 import com.amirnlz.core.network.dto.MovieListDto
-import com.amirnlz.core.network.error.ErrorHandler
 import javax.inject.Inject
 
 class MovieRemoteDataSource @Inject constructor(private val movieApiService: MovieApiService) {
 
-    suspend fun getPopularMovies(apiKey: String = ""): Result<MovieListDto> {
-        return try {
-            val response = movieApiService.getPopularMovies()
-            Result.success(response)
+    suspend fun getPopularMovies(apiKey: String = ""): MovieListDto {
+        try {
+            return movieApiService.getPopularMovies()
         } catch (e: Exception) {
-            Result.failure(ErrorHandler.handleException(e))
+            throw e
         }
     }
 
-    suspend fun getMovieDetails(movieId: Long, apiKey: String = ""): Result<MovieDetailsDto> {
+    suspend fun getMovieDetails(movieId: Long, apiKey: String = ""): MovieDetailsDto {
         return try {
-            val response = movieApiService.getMovieDetails(movieId = movieId)
-            Result.success(response)
+            movieApiService.getMovieDetails(movieId = movieId)
         } catch (e: Exception) {
-            Result.failure(ErrorHandler.handleException(e))
+            throw e
         }
     }
 
-    suspend fun getFavoriteMovies(accountId: Long): Result<MovieListDto> {
+    suspend fun getFavoriteMovies(accountId: Long): MovieListDto {
         return try {
-            val response = movieApiService.getFavoriteMovies(accountId)
-            Result.success(response)
+            movieApiService.getFavoriteMovies(accountId)
         } catch (e: Exception) {
-            Result.failure(ErrorHandler.handleException(e))
+            throw e
+
         }
     }
 
-    suspend fun addFavoriteMovie(accountId: Long, movieId: Long): Result<Unit> {
+    suspend fun addFavoriteMovie(accountId: Long, movieId: Long): Unit {
         return try {
-            val response = movieApiService.addFavoriteMovie(accountId, movieId)
-            Result.success(response)
+            movieApiService.addFavoriteMovie(accountId, movieId)
         } catch (e: Exception) {
-            Result.failure(ErrorHandler.handleException(e))
+            throw e
         }
     }
 }
--- core/data/movie/src/main/java/com/amirnlz/core/data/movie/repository/MovieRepositoryImpl.kt
+++ core/data/movie/src/main/java/com/amirnlz/core/data/movie/repository/MovieRepositoryImpl.kt
@@ -6,28 +6,55 @@ import com.amirnlz.core.data.movie.mapper.mapToMovieList
 import com.amirnlz.core.domain.movie.model.MovieDetails
 import com.amirnlz.core.domain.movie.model.MovieList
 import com.amirnlz.core.domain.movie.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MovieRepositoryImpl @Inject constructor(
     private val remoteDataSource: MovieRemoteDataSource
 ) : MovieRepository {
-    override suspend fun getPopularMovies(): Result<MovieList> {
-        return remoteDataSource.getPopularMovies()
-            .map { it.mapToMovieList() }
+    override suspend fun getPopularMovies(): Result<MovieList> = withContext(Dispatchers.IO) {
+        try {
+            val response = remoteDataSource.getPopularMovies()
+            return@withContext Result.success(response.mapToMovieList())
+        } catch (e: Exception) {
+            return@withContext Result.failure(e)
+        }
     }
 
     override suspend fun getMovieDetails(movieId: Long): Result<MovieDetails> {
-        return remoteDataSource.getMovieDetails(movieId = movieId)
-            .map { it.mapToMovieDetails() }
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = remoteDataSource.getMovieDetails(movieId = movieId)
+                return@withContext Result.success(response.mapToMovieDetails())
+            } catch (e: Exception) {
+                return@withContext Result.failure(e)
+            }
+        }
     }
 
     override suspend fun getFavoriteMovies(): Result<MovieList> {
-        return remoteDataSource.getFavoriteMovies(accountId = 1).map {
-            it.mapToMovieList()
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = remoteDataSource.getFavoriteMovies(accountId = 1)
+                return@withContext Result.success(response.mapToMovieList())
+            } catch (e: Exception) {
+                return@withContext Result.failure(e)
+            }
         }
     }
 
     override suspend fun toggleFavoriteMovie(movieId: Long): Result<Unit> {
-        return remoteDataSource.addFavoriteMovie(accountId = 1, movieId)
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = remoteDataSource.addFavoriteMovie(
+                    accountId = 1,
+                    movieId = movieId
+                )
+                return@withContext Result.success(response)
+            } catch (e: Exception) {
+                return@withContext Result.failure(e)
+            }
+        }
     }
 }
