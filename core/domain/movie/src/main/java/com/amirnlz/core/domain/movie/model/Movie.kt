4ab9289a0ca109868a7ffd76d9ee5000f6170f54
Amir <amiirnlz79@gmail.com>
2025-03-22 03:12:13 +0330

    Refactor: add nullable params to models

--- core/domain/movie/src/main/java/com/amirnlz/core/domain/movie/model/Movie.kt
+++ core/domain/movie/src/main/java/com/amirnlz/core/domain/movie/model/Movie.kt
@@ -5,14 +5,14 @@ data class Movie(
     val backdropPath: String? = null,
     val genreIDS: List<Long>?,
     val id: Long,
-    val originalLanguage: String?,
-    val originalTitle: String?,
+    val originalLanguage: String,
+    val originalTitle: String,
     val overview: String,
-    val popularity: Double,
+    val popularity: Double? = null,
     val posterPath: String? = null,
-    val releaseDate: String?,
-    val title: String,
-    val video: Boolean,
-    val voteAverage: Double,
-    val voteCount: Long
+    val releaseDate: String? = null,
+    val title: String? = null,
+    val video: Boolean? = null,
+    val voteAverage: Double? = null,
+    val voteCount: Long? = null
 )
