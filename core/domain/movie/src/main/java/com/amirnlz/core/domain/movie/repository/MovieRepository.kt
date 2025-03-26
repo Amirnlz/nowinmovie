92b107158aec43cbdbe42efa62e8e4f75ea4625e
Amir <amiirnlz79@gmail.com>
2025-03-26 17:19:04 +0330

    Feat: add other types of movie to domain & data module

--- core/domain/movie/src/main/java/com/amirnlz/core/domain/movie/repository/MovieRepository.kt
+++ core/domain/movie/src/main/java/com/amirnlz/core/domain/movie/repository/MovieRepository.kt
@@ -4,8 +4,11 @@ import com.amirnlz.core.domain.movie.model.MovieDetails
 import com.amirnlz.core.domain.movie.model.MovieList
 
 interface MovieRepository {
+    suspend fun getTrendingMovies(): Result<MovieList>
     suspend fun getPopularMovies(): Result<MovieList>
-    suspend fun getMovieDetails(movieId: Long): Result<MovieDetails>
+    suspend fun getTopRatedMovies(): Result<MovieList>
+    suspend fun getUpcomingMovies(): Result<MovieList>
     suspend fun getFavoriteMovies(): Result<MovieList>
+    suspend fun getMovieDetails(movieId: Long): Result<MovieDetails>
     suspend fun toggleFavoriteMovie(movieId: Long): Result<Unit>
 }
