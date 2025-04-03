cd184841922cfc9aff3ea2c505ddfd015dc694e1
Amir <amiirnlz79@gmail.com>
2025-04-04 00:13:56 +0330

    Feat: Add database module

---
+++ core/database/src/main/java/com/amirnlz/core/database/AppDatabase.kt
@@ -0,0 +1,18 @@
+package com.amirnlz.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.amirnlz.core.database.dao.FavoriteMovieDao
+import com.amirnlz.core.database.entity.MovieEntity
+import com.amirnlz.core.database.util.Converters
+
+@Database(
+    entities = [MovieEntity::class],
+    version = 1,
+    exportSchema = true,
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun favoriteMovieDao(): FavoriteMovieDao
+}
---
+++ core/database/src/main/java/com/amirnlz/core/database/util/Converters.kt
@@ -0,0 +1,34 @@
+package com.amirnlz.core.database.util
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+internal class Converters {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
+    private val gson = Gson()
+
+
+    @TypeConverter
+    fun fromLocalDate(date: LocalDate?): String? {
+        return date?.format(formatter)
+    }
+
+    @TypeConverter
+    fun toLocalDate(dateString: String?): LocalDate? {
+        return dateString?.let { LocalDate.parse(it, formatter) }
+    }
+
+    @TypeConverter
+    fun fromStringList(strings: List<String>?): String? {
+        return gson.toJson(strings)
+    }
+
+    @TypeConverter
+    fun toStringList(string: String?): List<String>? {
+        val listType = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(string, listType)
+    }
+}
