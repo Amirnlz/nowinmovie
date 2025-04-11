66531a47f7e53ea5322d5497ea9d84659a41f712
Amir <amiirnlz79@gmail.com>
2025-04-11 23:46:46 +0330

    Feat: add Spotless plugin configuration for code formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,5 +11,38 @@ plugins {
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.firebase.perf) apply false
     alias(libs.plugins.gms) apply false
+  alias(libs.plugins.spotless)
+}
 
+spotless {
+  kotlin {
+    target("**/*.kt")
+    trimTrailingWhitespace()
+    leadingTabsToSpaces()
+    endWithNewline()
+    ktlint("0.48.0")
+      .setEditorConfigPath("$projectDir/.editorconfig")
+      .editorConfigOverride(
+        mapOf(
+          "indent_size" to 2,
+          "ktlint_code_style" to "intellij_idea",
+        ),
+      )
+  }
+  kotlinGradle {
+    target("*.gradle.kts")
+    ktlint()
+  }
+  format("misc") {
+    target("**/*.gradle", "**/*.md", "**/.gitignore")
+    leadingTabsToSpaces()
+    trimTrailingWhitespace()
+    endWithNewline()
+  }
+  format("xml") {
+    target("**/*.xml")
+    leadingTabsToSpaces()
+    trimTrailingWhitespace()
+    endWithNewline()
+  }
 }
